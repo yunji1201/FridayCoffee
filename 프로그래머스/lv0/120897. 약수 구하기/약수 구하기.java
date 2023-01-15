d4f0565f31cb6468ccdd79c5ed07ea3d5ede85a4
yun <90806941+yunji1201@users.noreply.github.com>
2023-01-15 22:37:53 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList solution(int n) {
+        ArrayList answer = new ArrayList();
+        for (int i = 1; i < n + 1; i++) {
+            if (n % i == 0) {
+                answer.add(i);
+            }
+        }
+        return answer;
+    }
+}
