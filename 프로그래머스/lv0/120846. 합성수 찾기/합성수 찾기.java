10fdb98e40a5ea2a054afb788752f35252ef1dbf
yun <90806941+yunji1201@users.noreply.github.com>
2023-02-04 16:42:10 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 4; i <= n; i++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    answer++;
+                    j = (int) Math.sqrt(i);
+                }
+            }
+        }
+        return answer;
+    }
+}
