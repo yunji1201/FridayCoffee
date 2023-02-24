e4e090179b34a3eaccadf9da8cb37b026f60eb61
yunji1201 <ilovezltzot@naver.com>
2023-02-24 14:08:35 +0900

    공게임

---
+++ src/main/java/com/fridayCoffee/_68_gameMap/GameMap.java
@@ -0,0 +1,16 @@
+package com.fridayCoffee._68_gameMap;
+
+public class GameMap {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        int[][] numbers = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
+        System.out.println(solution.solution(numbers));
+    }
+}
+
+class Solution {
+    public int solution(int[][] maps) {
+        int answer = 0;
+        return answer;
+    }
+}
