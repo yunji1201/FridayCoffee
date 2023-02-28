48ed2ef1e8028842c0c77afe752234bbcb388b27
yun <ilovezltzot@naver.com>
2023-02-28 23:34:18 +0900

    game map 해석

---
+++ src/main/java/com/fridayCoffee/_69_controlZ/ControlZ.java
@@ -0,0 +1,16 @@
+package com.fridayCoffee._69_controlZ;
+
+public class ControlZ {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        int[] numbers = {1, 2, 3, 4, 5, 6};
+        System.out.println(solution.solution(numbers, 5));
+    }
+}
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+}
