ac409ec2b7712f8ffca5de0e844faf0bda1bbd87
yun <ilovezltzot@naver.com>
2023-03-01 23:53:16 +0900

    컨트롤 제트

--- src/main/java/com/fridayCoffee/_69_controlZ/ControlZ.java
+++ src/main/java/com/fridayCoffee/_69_controlZ/ControlZ.java
@@ -3,14 +3,18 @@ package com.fridayCoffee._69_controlZ;
 public class ControlZ {
     public static void main(String[] args) throws Exception {
         Solution solution = new Solution();
-        int[] numbers = {1, 2, 3, 4, 5, 6};
-        System.out.println(solution.solution(numbers, 5));
+//        String[] numbers = {"10 Z 20 Z 1"};
+        System.out.println(solution.solution("10 Z 20 Z 1"));
     }
 }
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        String[] arr = s.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            answer = !arr[i].equals("Z") ? (answer += Integer.parseInt(arr[i])) : (answer -= Integer.parseInt(arr[i - 1]));
+        }
         return answer;
     }
 }
