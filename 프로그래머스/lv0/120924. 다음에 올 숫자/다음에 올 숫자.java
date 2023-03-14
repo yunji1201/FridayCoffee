aba7a6c190debd9232584408cf699a4bddc6054e
yun <90806941+yunji1201@users.noreply.github.com>
2023-03-14 13:56:28 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int[] common) {
-        int answer = 0;
         int d = common[1] - common[0];
         int last = common[common.length - 1];
-        answer = d == common[2] - common[1] ? last + d : last *  common[1] / common[0];
-        return answer;
+        return d == common[2] - common[1] ? last + d : last *  common[1] / common[0];
     }
 }
