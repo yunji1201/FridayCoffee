ad92722d0b747ec9c5f71b39b10922aff8708842
yun <90806941+yunji1201@users.noreply.github.com>
2023-03-29 15:53:11 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        while (num != 1) {
+            num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+            answer++;
+        }
+
+        if (answer > 500) return -1;
+        return answer;
+    }
+}
