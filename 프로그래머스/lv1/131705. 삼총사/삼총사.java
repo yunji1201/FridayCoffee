a3b8d2820ac9abe2222b174e4c7778b9e3048440
yun <90806941+yunji1201@users.noreply.github.com>
2023-03-30 21:34:54 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,18 +1,27 @@
 class Solution {
+    int answer = 0;
+    int student = 0;
+    int start = 0;
+    int[] selected = new int[3];
+
     public int solution(int[] number) {
-        int answer = 0;
-        int first = 0;
-        
-        while (first <= number.length-2) {
-            for (int i = first + 1; i < number.length; i++) {
-                for (int j = i + 1; j < number.length; j++) {
-                    if (number[first] + number[i] + number[j] == 0) {
-                        answer++;
-                    }
-                }
-            }
-            first++;
-        }
+
+        dfs(number, student, selected, start);
         return answer;
+    }
+
+    private void dfs(int[] number, int student, int[] selected, int start) {
+        if (student == 3) {
+            if (selected[0] + selected[1] + selected[2] == 0) {
+                answer++;
+            }
+            return;
+        }
+
+        for (int i = start; i < number.length; i++) {
+            selected[student] = number[i];
+            dfs(number, student + 1, selected, i + 1);
+            selected[student] = 0;
+        }
     }
 }
