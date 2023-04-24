f4c7076f9ed9ad9e450b186f6e7d517c0955e568
yjkim_1201 <yjkim@wemb.co.kr>
2023-04-24 13:50:30 +0900

    pid 찾기

---
+++ src/main/java/com/fridayCoffee/_89_network/Network.java
@@ -0,0 +1,18 @@
+package com.fridayCoffee._89_network;
+
+public class Network {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        int[][] numbers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        System.out.println("======================================");
+        System.out.println("my answer : " + solution.solution(3, numbers)); // 2
+        System.out.println("======================================");
+    }
+}
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        return answer;
+    }
+}
--- src/main/java/com/fridayCoffee/fridayCoffeeApplication.java
+++ src/main/java/com/fridayCoffee/fridayCoffeeApplication.java
@@ -3,11 +3,46 @@ package com.fridayCoffee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.management.ManagementFactory;
+import java.util.Map;
+
 @SpringBootApplication
 public class fridayCoffeeApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(fridayCoffeeApplication.class, args);
+
+        System.out.println("//////////////////////////////////////////////");
+        System.out.println("my pid is " + ManagementFactory.getRuntimeMXBean().getName().split("@")[0]);
+        System.out.println("//////////////////////////////////////////////");
+
+        // 자식 프로세스를 실행할 명령어와 인자를 지정
+        ProcessBuilder pb = new ProcessBuilder("command", "arg1", "arg2");
+
+        // 작업 디렉토리를 설정할 수 있다
+        pb.directory(new File("/path/to/directory"));
+
+        // 환경 변수를 설정할 수 있다
+        Map<String, String> env = pb.environment();
+        env.put("KEY1", "VALUE1");
+        env.put("KEY2", "VALUE2");
+
+        // 자식 프로세스를 실행
+        Process process = pb.start();
+
+        // 자식 프로세스의 입력, 출력, 에러 스트림을 다룰 수 있다
+        InputStream inputStream = process.getInputStream();
+        OutputStream outputStream = process.getOutputStream();
+        InputStream errorStream = process.getErrorStream();
+
+        // 자식 프로세스의 상태 코드를 얻을 수 있다
+        int exitCode = process.waitFor();
+
+        System.out.println("자식 프로세스 종료 코드 : " + exitCode);
     }
 
 }
