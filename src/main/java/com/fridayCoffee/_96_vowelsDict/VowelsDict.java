df58d4851194f780ca506b6f4d054ce780a1fbae
yun <ilovezltzot@naver.com>
2023-05-15 00:34:25 +0900

    모음사전

--- src/main/java/com/fridayCoffee/_96_vowelsDict/VowelsDict.java
+++ src/main/java/com/fridayCoffee/_96_vowelsDict/VowelsDict.java
@@ -42,4 +42,105 @@ class Solution {
     }
 }
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/84512
+class Solution1 {
+    public int solution(String word) {
+        int answer = 0, per = 3905;
+        for (String s : word.split("")) answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
+        return answer;
+    }
+}
+
+class Solution2 {
+    List<String> list = new ArrayList<>();
+
+    void dfs(String str, int len) {
+        if (len > 5) return;
+        list.add(str);
+        for (int i = 0; i < 5; i++) dfs(str + "AEIOU".charAt(i), len + 1);
+    }
+
+    public int solution(String word) {
+        dfs("", 0);
+        return list.indexOf(word);
+    }
+}
+
+class Solution3 {
+    public int solution(String word) {
+        int answer = word.length();
+        int pos;
+        for (pos = 0; pos < word.length(); pos++) {
+            char c = word.charAt(pos);
+            if (c == 'A') continue;
+            int temp = 0;
+            for (int i = 0; i <= 4 - pos; i++) {
+                temp += Math.pow(5, i);
+            }
+            answer += temp * val(c);
+        }
+        return answer;
+    }
+
+    public int val(char c) {
+        if (c == 'E') return 1;
+        else if (c == 'I') return 2;
+        else if (c == 'O') return 3;
+        else return 4;
+    }
+}
+
+class Solution4 {
+
+    static String alpabet = "AEIOU";
+    static String[] dict = alpabet.split("");
+    static int result = 0;
+    static int count = 0;
+
+    public int solution(String word) {
+        StringBuilder sb = new StringBuilder();
+        dfs(5, 0, sb, word);
+        return result;
+
+    }
+
+    void dfs(int target, int cnt, StringBuilder sb, String word) {
+        if (sb.toString().equals(word)) {
+            result = count;
+            return;
+        }
+
+        if (target == cnt) {
+            return;
+        }
+
+        for (int i = 0; i < dict.length; i++) {
+            sb.append(dict[i]);
+            count++;
+            dfs(target, cnt + 1, sb, word);
+            sb.deleteCharAt(sb.length() - 1);
+
+        }
+
+    }
+
+}
+
+class Solution5 {
+    public int solution(String word) {
+        int answer = 0;
+        int mul = 781;
+
+        char chr[] = {'A', 'E', 'I', 'O', 'U'};
+
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 0; j < 5; j++) {
+                if (chr[j] == word.charAt(i)) {
+                    answer += 1 + j * mul;
+                }
+            }
+            mul = (mul - 1) / 5;
+        }
+
+        return answer;
+    }
+}
