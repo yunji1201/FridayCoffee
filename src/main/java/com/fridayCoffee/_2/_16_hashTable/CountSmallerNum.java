e305739f39b5ad364e0b73569a5e2211e8591984
yun <ilovezltzot@naver.com>
2023-08-29 23:53:50 +0900

    hashtable 푸는중

---
+++ src/main/java/com/fridayCoffee/_2/_16_hashTable/CountSmallerNum.java
@@ -0,0 +1,22 @@
+package com.fridayCoffee._2._16_hashTable;
+
+public class CountSmallerNum {
+    /*  1365. How Many Numbers Are Smaller Than the Current Number
+        Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+        That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+        Return the answer in an array.
+      */
+
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        System.out.println("======================================");
+        int[] prices = {1, 2, 3, 2, 3};
+        System.out.println("my answer : " + solution.solution(prices)); // [4, 3, 1, 1, 0]
+    }
+}
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+    }
+}
