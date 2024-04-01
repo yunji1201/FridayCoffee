589be7a89a16fefd5b85847e311617b1ce6f719d
yjkim <yjkim@wemb.com>
2024-04-01 18:29:23 +0900

    피보나치 푸는중

---
+++ src/main/java/com/fridayCoffee/_2/_24_Fibonacci/Fibonacci.java
@@ -0,0 +1,20 @@
+package com.fridayCoffee._2._24_Fibonacci;
+
+public class Fibonacci {
+
+    public void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        System.out.println("======================================");
+        int[] citations = {3, 0, 6, 1, 5};
+        System.out.println("my answer : " + solution.solution(citations)); // 3
+    }
+
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12945
