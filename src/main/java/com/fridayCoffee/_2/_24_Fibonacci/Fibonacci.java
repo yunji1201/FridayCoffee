105e05d28c713b6526f0829711360888f60ec538
yjkim <yjkim@wemb.com>
2024-04-01 18:50:14 +0900

    피보나치 푸는중

--- src/main/java/com/fridayCoffee/_2/_24_Fibonacci/Fibonacci.java
+++ src/main/java/com/fridayCoffee/_2/_24_Fibonacci/Fibonacci.java
@@ -5,13 +5,26 @@ public class Fibonacci {
     public void main(String[] args) throws Exception {
         Solution solution = new Solution();
         System.out.println("======================================");
-        int[] citations = {3, 0, 6, 1, 5};
-        System.out.println("my answer : " + solution.solution(citations)); // 3
+        System.out.println("my answer : " + solution.solution(3)); // 3
     }
 
     class Solution {
         public int solution(int n) {
             int answer = 0;
+
+            if (n <= 1) return n;
+
+            int prevOne = 1;
+            int prevTwo = 2;
+
+            for (int i = 3; i <= n; i++) {
+                answer = prevOne + prevTwo;
+                prevTwo = prevOne;
+                prevOne = answer;
+            }
+
+            System.out.println(answer);
+            answer = answer % 1234567;
             return answer;
         }
     }
