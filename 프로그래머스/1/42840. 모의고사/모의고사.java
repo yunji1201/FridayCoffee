f491ad4caf691692cce37f29487d75c0c2c9b0cf
yun <90806941+yunji1201@users.noreply.github.com>
2024-04-04 23:55:24 +0900

    [level 1] Title: 모의고사, Time: 2.83 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] answers) {
+        
+        int[][] patterns = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        
+        int[] scores = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < patterns.length; j++) {
+                if (answers[i] == patterns[j][i % patterns[j].length]) {
+                    scores[j]++;
+                }
+            }
+        }
+
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
+        
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == maxScore) {
+                list.add(i + 1);
+            }
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
