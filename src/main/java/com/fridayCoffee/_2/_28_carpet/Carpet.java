287499326761183c6a9a1873235e344186f2ccf6
yjkim <yjkim@wemb.com>
2024-04-11 16:30:58 +0900

    카펫

---
+++ src/main/java/com/fridayCoffee/_2/_28_carpet/Carpet.java
@@ -0,0 +1,18 @@
+package com.fridayCoffee._2._28_carpet;
+
+public class Carpet {
+
+    public void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        System.out.println("======================================");
+        System.out.println("my answer : " + solution.solution(10, 2)); // [4,3]
+    }
+
+    // 내 풀이
+    class Solution {
+        public int[] solution(int brown, int yellow) {
+            int[] answer = {};
+            return answer;
+        }
+    }
+}
