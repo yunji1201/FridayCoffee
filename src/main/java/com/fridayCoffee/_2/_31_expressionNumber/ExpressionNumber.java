ed4199331488f1425ab1a3721b852198c46b5308
yjkim <yjkim@wemb.com>
2024-04-19 17:02:59 +0900

    12924

---
+++ src/main/java/com/fridayCoffee/_2/_31_expressionNumber/ExpressionNumber.java
@@ -0,0 +1,26 @@
+package com.fridayCoffee._2._31_expressionNumber;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExpressionNumber {
+
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        System.out.println("======================================");
+        System.out.println("my answer : " + solution.solution(15)); // 4
+    }
+}
+
+// 내 풀이
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+// 다른 사람들 풀이
+
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12924
