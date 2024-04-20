6aa7d47221a0a3a54855410917fe04e7050a7cb1
yun <90806941+yunji1201@users.noreply.github.com>
2024-04-20 22:24:57 +0900

    숫자의 표현

--- src/main/java/com/fridayCoffee/_2/_31_expressionNumber/ExpressionNumber.java
+++ src/main/java/com/fridayCoffee/_2/_31_expressionNumber/ExpressionNumber.java
@@ -46,6 +46,9 @@ class Solution1 {
         return answer;
     }
 }
+// 문제 해석
+    // 주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 홀수 약수의 개수와 같다라는 정수론 정리
+    // 홀수의 약수 개수르 세는 방식
 
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/12924
